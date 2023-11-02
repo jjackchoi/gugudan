7ff6787f280d1b161e8ae2754a59f180b03a9146
jjackchoi <jackchoi0325@gmail.com>
2023-11-02 18:13:07 +0900

    Scanner를 이용한 구구단 반복문 구현

--- src/gugudan/Gugudan.java
+++ src/gugudan/Gugudan.java
@@ -1,38 +1,31 @@
 package gugudan;
 
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Gugudan {
 	public static void main(String[] args) {
-		//4단
-		System.out.println("4단");
-		int result = 4*1;
-		System.out.println(result);
-		result = 4*2;
-		System.out.println(result);
-		result = 4*3;
-		System.out.println(result);
-		result = 4*4;
-		System.out.println(result);
-		result = 4*5;
-		System.out.println(result);
-		result = 4*6;
-		System.out.println(result);
-		result = 4*7;
-		System.out.println(result);
 		//5단
+//		System.out.println("구구단을 출력할 단은?");
+//		Scanner scanner = new Scanner(System.in);
+//		int number = scanner.nextInt();
+//		System.out.println(number*1);
+//		System.out.println(number*2);
+//		System.out.println(number*3);
+//		System.out.println(number*4);
+//		System.out.println(number*5);
+//		System.out.println(number*6);
+//		System.out.println(number*7);
+//		System.out.println(number*8);
+//		System.out.println(number*9);
+		//6단
 		System.out.println("구구단을 출력할 단은?");
 		Scanner scanner = new Scanner(System.in);
 		int number = scanner.nextInt();
-		System.out.println(number*1);
-		System.out.println(number*2);
-		System.out.println(number*3);
-		System.out.println(number*4);
-		System.out.println(number*5);
-		System.out.println(number*6);
-		System.out.println(number*7);
-		System.out.println(number*8);
-		System.out.println(number*9);
+		for (int i = 1; i < 10; i++) {
+			int result = number*i;
+			System.out.println(result);
+		}
 		
 	}
 }
