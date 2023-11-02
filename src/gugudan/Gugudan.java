e8cee079a93d9eff7cbc1685877949d64fc0c922
DESKTOP-DNFB9KT\choi <jackchoi0325@gmail.com>
2023-11-03 02:43:08 +0900

    gugudan 구현

--- src/gugudan/Gugudan.java
+++ src/gugudan/Gugudan.java
@@ -5,30 +5,16 @@ import java.util.Scanner;
 
 public class Gugudan {
 	public static void main(String[] args) {
-		//5단
-//		System.out.println("구구단을 출력할 단은?");
-//		Scanner scanner = new Scanner(System.in);
-//		int number = scanner.nextInt();
-//		System.out.println(number*1);
-//		System.out.println(number*2);
-//		System.out.println(number*3);
-//		System.out.println(number*4);
-//		System.out.println(number*5);
-//		System.out.println(number*6);
-//		System.out.println(number*7);
-//		System.out.println(number*8);
-//		System.out.println(number*9);
-		
-		//6단
-		int i = 1;
-		while(i < 9) {
-			System.out.println(6*i);
-			i ++;
-		}
-		//7단
-		for(int j = 1; j <10; j++) {
-			System.out.println(7*j);
-		}
+//		//6단
+//		int i = 1;
+//		while(i < 9) {
+//			System.out.println(6*i);
+//			i ++;
+//		}
+//		//7단
+//		for(int j = 1; j <10; j++) {
+//			System.out.println(7*j);
+//		}
 //		System.out.println("구구단을 출력할 단은?");
 //		Scanner scanner = new Scanner(System.in);
 //		int number = scanner.nextInt();
@@ -36,6 +22,21 @@ public class Gugudan {
 //			int result = number*i;
 //			System.out.println(result);		
 //		}
+		//8,9단
+		System.out.println("구구단 중 출력할 단은?");
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		System.out.println("사용자가 입력한 값 : "+ num);
+		if (num<2) {
+			System.out.println("값을 잘못 입력했습니다");
+		}else if(num > 9) {
+			System.out.println("값을 잘못 입력했습니다.");
+		}else {
+			for(int i = 1; i < 10; i++) {
+				System.out.println(num * i);
+			}
+		}
+		
 		
 	}
 }
