c831e18972bdd1cfbaa9f4828410ca44398c2187
DESKTOP-DNFB9KT\choi <jackchoi0325@gmail.com>
2023-11-03 02:27:45 +0900

    for문 7단구현

--- src/gugudan/Gugudan.java
+++ src/gugudan/Gugudan.java
@@ -18,14 +18,24 @@ public class Gugudan {
 //		System.out.println(number*7);
 //		System.out.println(number*8);
 //		System.out.println(number*9);
+		
 		//6단
-		System.out.println("구구단을 출력할 단은?");
-		Scanner scanner = new Scanner(System.in);
-		int number = scanner.nextInt();
-		for (int i = 1; i < 10; i++) {
-			int result = number*i;
-			System.out.println(result);
+		int i = 1;
+		while(i < 9) {
+			System.out.println(6*i);
+			i ++;
 		}
+		//7단
+		for(int j = 1; j <10; j++) {
+			System.out.println(7*j);
+		}
+//		System.out.println("구구단을 출력할 단은?");
+//		Scanner scanner = new Scanner(System.in);
+//		int number = scanner.nextInt();
+//		for (int i = 1; i < 10; i++) {
+//			int result = number*i;
+//			System.out.println(result);		
+//		}
 		
 	}
 }
